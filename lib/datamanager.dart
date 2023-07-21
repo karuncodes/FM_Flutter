e1d0301ebc639665a28a4f73cdef058c78f5ea32
Karun Kamal <kkamal@zendesk.com>
2023-07-21 12:00:07 +0530

    added http data request to get items

--- lib/datamanager.dart
+++ lib/datamanager.dart
@@ -1,8 +1,32 @@
+import 'dart:convert';
+
 import 'datamodel.dart';
+import 'package:http/http.dart' as http;
 
 class DataManager {
   List<Category>? _menu;
   List<ItemsInCart> cart = [];
+
+  getMenu() async {
+    const url = "https://firtman.github.io/coffeemasters/api/menu.json";
+    var response = await http.get(Uri.parse(url));
+    if(response.statusCode == 200) {
+      var body = response.body;
+      var decodedData = jsonDecode(body) as List<dynamic>;
+      for (var json in decodedData) {
+        _menu?.add(Category.fromJson(json));
+      }
+    }
+  }
+
+  Future<List<Category>> fetchMenu() async {
+    if(_menu == null) {
+      await getMenu();
+    }
+
+    return _menu!;
+
+  }
 
   cartAdd (Product p) {
     bool found = false; 
