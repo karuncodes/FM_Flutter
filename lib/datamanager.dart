95bbcf0ee1d862bd92595af24b9378b86fd51359
Karun Kamal <kkamal@zendesk.com>
2023-07-21 20:48:31 +0530

    correction on adding item to cart logic

--- lib/datamanager.dart
+++ lib/datamanager.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class DataManager {
   List<Category>? _menu;
-  List<ItemsInCart> cart = [];
+  List<ItemInCart> cart = [];
 
   fetchMenu() async {
     const url = "https://firtman.github.io/coffeemasters/api/menu.json";
@@ -28,17 +28,16 @@ class DataManager {
   }
 
   cartAdd (Product p) {
-    bool found = false; 
+    bool found = false;
     for (var item in cart) {
       if (item.product.id == p.id) {
         item.quantity++;
         found = true;
       }
-
-      if(!!found) {
-        cart.add(ItemsInCart(product: p, quantity: 1));
-      }
     }
+      if(!found) {
+        cart.add(ItemInCart(product: p, quantity: 1));
+      }
   }
 
   cartRemove (Product p) {
